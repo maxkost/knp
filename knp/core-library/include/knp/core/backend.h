1f10154e89d986f0eb5685855e6cf3ce97e4fe96
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-01-27 14:34:43 +0300

    Docstrings and interface refactoring

--- knp/core-library/include/knp/core/backend.h
+++ knp/core-library/include/knp/core/backend.h
@@ -10,6 +10,7 @@
 #include <knp/core/core.h>
 #include <knp/core/device.h>
 #include <knp/core/message_bus.h>
+
 #include <vector>
 
 
@@ -22,44 +23,116 @@ namespace knp::core
 class Backend
 {
 public:
+    /**
+     * Get this backend UID.
+     * @return UID.
+     */
     [[nodiscard]] const UID &get_uid() const { return base_.uid_; }
+    /**
+     * Get this backend tags.
+     * @return tag map.
+     * @see TagMap.
+     */
     [[nodiscard]] auto &get_tags() { return base_.tags_; }
 
 public:
-    /*
+    /**
      * @brief load populations to the backend.
-     * @tparam PopulationType population type, if supported.
+     * @tparam PopulationType population type, if supported by the backend.
      * @param populations vector to load.
      */
     template <typename PopulationType>
     void load_populations(const std::vector<PopulationType> &populations);
+
+    /**
+     * @brief load populations to the backend.
+     * @tparam PopulationType population type, if supported by the backend.
+     * @param populations vector to load.
+     */
     template <typename PopulationType>
     void load_populations(std::vector<PopulationType> &&populations);
 
-    /*
+    /**
      * @brief load projections to the backend.
-     * @tparam ProjectionType projection type, if supported.
+     * @tparam ProjectionType projection type, if supported by the backend.
      * @param projections vector to load.
      */
     template <typename ProjectionType>
     void load_projections(const std::vector<ProjectionType> &projections);
+
+    /**
+     * @brief load projections to the backend.
+     * @tparam ProjectionType projection type, if supported by the backend.
+     * @param projections vector to load.
+     */
     template <typename ProjectionType>
     void load_projections(const std::vector<ProjectionType> &&projections);
 
 public:
+    /**
+     * Remove projections with given UIDs from the backend.
+     * @param uids identifiers of the projections, which will be removed.
+     */
+
     void remove_projections(const std::vector<UID> &uids);
+    /**
+     * Remove synapses from the projection with given UID from the backend.
+     * @param projection_uid identifiers of the projection.
+     * @param indexes synapses indexes to remove.
+     */
+
     void remove_synapses(const UID &projection_uid, const std::vector<size_t> &indexes);
+    /**
+     * Remove populations with given UIDs from the backend.
+     * @param uids identifiers of the populations, which will be removed.
+     */
+
     void remove_populations(const std::vector<UID> &uids);
+
+    /**
+     * Remove neurons from the population with given UID from the backend.
+     * @param population_uid identifiers of the population.
+     * @param indexes neurons indexes to remove.
+     */
     void remove_neurons(const UID &population_uid, const std::vector<size_t> &indexes);
 
 public:
-    std::vector<Device> &&get_devices() const;
+    /**
+     * Get devices list, supported by the backend.
+     * @return list of the devices.
+     * @see Device.
+     */
+
+    [[nodiscard]] std::vector<Device> &&get_devices() const;
+
+    /**
+     * Get device which
+     * @return
+     */
     Device &get_current_device() const;
-    void select_device();
+
+    /**
+     * Select device, where backend will work.
+     * @param uid of the device which the backend uses.
+     */
+    void select_device(const UID &uid);
 
 public:
+    /**
+     * Start execution.
+     */
     void start();
+
+    /**
+     * Stop execution.
+     */
     void stop();
+
+    /**
+     * Make one execution step.
+     *
+     * Need for debugging.
+     */
     void step();
 
 public:
--- knp/core-library/include/knp/core/projection.h
+++ knp/core-library/include/knp/core/projection.h
@@ -8,12 +8,13 @@
 
 #pragma once
 
-#include <vector>
+#include <knp/core/core.h>
+#include <knp/core/uid.h>
+#include <knp/synapse-traits/type_traits.h>
+
 #include <functional>
 #include <tuple>
-#include "knp/core/uid.h"
-#include "knp/core/core.h"
-#include "knp/synapse-traits/type_traits.h"
+#include <vector>
 
 namespace knp::core
 {
@@ -22,14 +23,12 @@ namespace knp::core
  * @brief The Projection class is definition of the similar connections between neurons of two populations.
  * @note I think this class should later be divided to interface and implementation classes.
  */
-
-template<class ItemClass>
+template <class ItemClass>
 class Projection
 {
 public:
     using SynapseParameters = typename synapse_traits::synapse_parameters<ItemClass>::SynapseParameters;
     using SynapseGenerator = std::function<ItemClass(size_t)>;
-
 
     Projection(size_t starting_size, SynapseGenerator generator)
     {
@@ -40,63 +39,70 @@ public:
         }
     }
 
+public:
+    /**
+     * Get this projection UID.
+     * @return UID.
+     */
+    [[nodiscard]] const UID &get_uid() const { return base_.uid_; }
+    /**
+     * Get this projection tags.
+     * @return tag map.
+     * @see TagMap.
+     */
+    [[nodiscard]] auto &get_tags() { return base_.tags_; }
 
+public:
     /**
      * Calculates synapse parameters based on its index and its ItemClass
-     * @param n index of the synapse
+     * @param synapse_index index of the synapse
      * @return a container of synapse parameters
      */
-    SynapseParameters get_synapse_parameters(size_t n) { return parameters_[n]; }
-
+    [[nodiscard]] SynapseParameters &get_synapse_parameters(size_t synapse_index) const
+    {
+        return parameters_[synapse_index];
+    }
 
     /**
      * Calculates (if needed) and returns all the synaptic parameters for the projection
      * @return vector of parameters, one structure for each synapse
      */
-    const std::vector<SynapseParameters>& get_synapses_parameters() { return parameters_; }
-
+    [[nodiscard]] const std::vector<SynapseParameters> &get_synapses_parameters() const { return parameters_; }
 
     /**
      * Gets the number of synapses
      * @return number of synapses inside the projection
      */
-    size_t get_size() { return parameters_.size(); }
-
+    [[nodiscard]] size_t get_size() { return parameters_.size(); }
 
     /**
      * Sets new synapse parameter
-     * @param new_parameters
-     * @param index
+     * @param new_parameters new synapse parameters.
+     * @param synapse_index index of the synapse.
      */
-    void set_synapse_parameter(SynapseParameters new_parameters, size_t index)
+    void set_synapse_parameter(SynapseParameters &&new_parameters, size_t index)
     {
-        parameters_[index] == std::move(new_parameters);
+        parameters_[index] == new_parameters;
     }
-
-    /**
-     * A function to return tags associated with population
-     * @return tag container
-     */
-    TagMap get_tags() { return base_.tags_; }
 
     /**
      * Returns the UID of population this projection gets signals from
      * @return presynaptic population UID
      */
-    UID get_presynaptic() { return presynaptic_uid_; }
+    [[nodiscard]] UID get_presynaptic() const { return presynaptic_uid_; }
 
     /**
-    * Returns the UID of population this projection sends signals to
-    * @return postsynaptic population UID
-    */
-    UID get_postsynaptic() { return postsynaptic_uid_; }
+     * Returns the UID of population this projection sends signals to
+     * @return postsynaptic population UID
+     */
+    [[nodiscard]] UID get_postsynaptic() const { return postsynaptic_uid_; }
 
     /**
      * Calculates connection parameters for a given synapse index
      * @param index synapse index
      * @return presynaptic neuron index, synapse index, postsynaptic neuron index
      */
-    std::tuple<size_t, size_t, size_t> get_connection(size_t index)
+    [[nodiscard]] std::tuple<size_t, size_t, size_t> &get_connection(size_t index) const
     {
         return std::make_tuple(parameters_[index].pre_id, index, parameters_[index].post_id);
     }
@@ -104,15 +110,30 @@ public:
     /**
      * @brief calculates connections parameters from data
      */
-    std::vector<std::tuple<size_t, size_t, size_t>> get_connections();
+    [[nodiscard]] const std::vector<std::tuple<size_t, size_t, size_t>> &get_connections() const;
 
+public:
+    /**
+     * Reset synapses weights.
+     */
+    void reset_weights();
+
+    /**
+     * Lock synapses weights.
+     */
+    void lock_weights();
+
+    /**
+     * Unlock synapses weights.
+     */
+    void unlock_weights();
 
 private:
     BaseData base_;
     UID presynaptic_uid_;
     UID postsynaptic_uid_;
 
-    // Parameters container
+    /// Parameters container.
     std::vector<SynapseParameters> parameters_;
 };
 
