eb01366e1292a2e54fba9f13c9b9c91bcd82561d
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-01-30 13:36:30 +0300

    CPU backend directory added

---
+++ knp/core-library/impl/device.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file backend.cpp
+ * @brief Backend base class implementation.
+ * @author Artiom N.
+ * @date 11.01.2023
+ */
+
+
+#include <knp/core/backend.h>
+
+
+namespace knp::core
+{
+
+}  // namespace knp::core
