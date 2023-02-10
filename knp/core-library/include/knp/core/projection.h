ea7ddda31539edcbb4f8261780abf034c2007e8e
Andrey Vartenkov <a.vartenkov@not-a-real-email.notreal>
2023-02-10 16:53:43 +0300

    add const

--- knp/core-library/include/knp/core/projection.h
+++ knp/core-library/include/knp/core/projection.h
@@ -161,7 +161,7 @@ public:
      */
     size_t add_synapses(size_t num_iterations, SynapseGenerator &generator)
     {
-        size_t starting_size = parameters_.size();
+        const size_t starting_size = parameters_.size();
         for (size_t i = 0; i < num_iterations; ++i)
         {
             if (auto data = std::move(generator(i)))
