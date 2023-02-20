0a3d8385051c87062c2c435d0cc7f65a4eaf379b
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-02-20 17:44:32 +0300

    First CPU power consumption meter implemented

--- knp/devices-library/impl/cpu.cpp
+++ knp/devices-library/impl/cpu.cpp
@@ -8,8 +8,29 @@
 
 #include <knp/devices/cpu.h>
 
+#include "cpu_power.h"
+
 
 namespace knp::devices
 {
 
+CPU::CPU() : power_meter_{std::make_unique<CpuPower>()} {}
+
+
+knp::core::DeviceType CPU::get_type() const
+{
+    return knp::core::DeviceType::CPU;
+}
+
+
+const std::string &CPU::get_name() const
+{
+    return "CPU0";
+}
+
+
+const float CPU::get_power() const
+{
+    return power_meter_->get_power();
+}
 }  // namespace knp::devices
--- knp/devices-library/include/knp/devices/cpu.h
+++ knp/devices-library/include/knp/devices/cpu.h
@@ -9,10 +9,41 @@
 
 #include <knp/core/device.h>
 
+#include <memory>
+#include <string>
+
 
 namespace knp::devices
 {
+
+// Need to store PCM instance pointer.
+class CpuPower;
+
 class CPU : public knp::core::Device
 {
+public:
+    CPU();
+
+public:
+    /**
+     * @brief Get current device type.
+     * @return device type.
+     */
+    [[nodiscard]] knp::core::DeviceType get_type() const override;
+
+    /**
+     * @brief Get device name, i.e. CPU0.
+     * @return device name in the arbitrary format.
+     */
+    [[nodiscard]] const std::string &get_name() const override;
+
+    /**
+     * @brief Get device power consumption.
+     * @return power consumption.
+     */
+    [[nodiscard]] const float get_power() const override;
+
+private:
+    mutable std::unique_ptr<CpuPower> power_meter_;
 };
 }  // namespace knp::devices
