5568b700bc1e7b36cbb30b7a8d175e5b14fa3e8b
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-03-13 23:19:57 +0300

    Doxygen icons changed

---
+++ knp/autodoc/js/icons-add-properties.js
@@ -0,0 +1,6 @@
+// Need to add properties to the spans, to select them in CSS.
+
+$.add_icon_classes = function()
+{
+    $('span.icon').each(function() { $(this).addClass("icon-" + $(this).text()) })
+}
