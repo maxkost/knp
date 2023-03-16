8b7a407f39fd9d9f18933192b01e3986db07034c
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-03-16 16:48:43 +0300

    Backend loader stub implemented

---
+++ knp/base-framework/impl/backend_loader.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file backend_loader.cpp
+ * @brief Backends loader implementation.
+ * @author Artiom N.
+ * @date 16.03.2023
+ */
+
+#include <knp/framework/backend_loader.h>
---
+++ knp/base-framework/include/knp/framework/backend_loader.h
@@ -0,0 +1,22 @@
+/**
+ * @file backend_loader.h
+ * @brief Backends loader interface.
+ * @author Artiom N.
+ * @date 16.03.2023
+ */
+
+#pragma once
+
+#include <knp/core/uid.h>
+
+#include <any>
+#include <map>
+#include <string>
+#include <variant>
+
+
+/// Framework namespace.
+namespace knp::framework
+{
+
+}  // namespace knp::framework
