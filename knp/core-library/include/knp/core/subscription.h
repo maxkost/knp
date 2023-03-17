34f1273b443ea6ece01599ce7cbd79f876dea669
Andrey Vartenkov <a.vartenkov@not-a-real-email.notreal>
2023-03-17 12:56:00 +0300

    Finish subscription, add comments

--- knp/core-library/include/knp/core/subscription.h
+++ knp/core-library/include/knp/core/subscription.h
@@ -18,8 +18,8 @@ namespace knp::core
 {
 
 /**
- * @brief A subscription class for message exchange among network entities
- * @tparam MessageType
+ * @brief A subscription class used for message exchange among network entities
+ * @tparam MessageType type of messages
  */
 template <class MessageType>
 class Subscription
@@ -30,20 +30,39 @@ public:
     {
     }
 
+    /// Get a set of sender UIDs
     const auto &get_senders() { return senders_; }
+
+    /// Get receiver UID
     UID get_receiver() { return receiver_; }
 
     /**
-     * Unsubscribe from a sender. If not subscribed to the sender, do nothing.
+     * @brief Unsubscribe from a sender. If not subscribed to the sender, do nothing.
      * @param uid sender UID
+     * @return 1 if unsubscribed, 0 if no sender was found
      */
     size_t remove_sender(const UID &uid) { return senders_.erase(uid); }
 
-    bool has_sender() {}
+    /**
+     * @brief Add an additional sender to the subscription
+     * @param uid new sender UID
+     * @return number of senders added (0 if already subscribed, 1 otherwise)
+     */
+    size_t add_sender(const UID &uid) { return senders_.insert(uid).second; }
+
+    /**
+     * @brief Checks if a sender exists
+     * @param uid sender UID
+     * @return true if sender exists
+     */
+    bool has_sender(const UID &uid) { return senders_.count(uid); }
 
 private:
+    /// Set of sender UIDs
     std::unordered_set<UID, UID_hash> senders_;
+    /// Receiver UID
     UID receiver_;
+    /// Message cache
     std::vector<MessageType> messages_;
 };
 
