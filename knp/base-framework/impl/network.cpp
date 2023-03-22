0530cf68d5a948f1b5b57a30b94cdeaac3c9190a
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-03-22 14:33:38 +0300

    Network class stub added

---
+++ knp/base-framework/impl/network.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file network.cpp
+ * @brief Network class implementation.
+ * @author Artiom N.
+ * @date 22.03.2023
+ */
+
+#include <knp/framework/network.h>
+
+#include <spdlog/spdlog.h>
+
+
+namespace knp::framework
+{
+
+}  // namespace knp::framework
---
+++ knp/base-framework/include/knp/framework/network.h
@@ -0,0 +1,23 @@
+/**
+ * @file network.h
+ * @brief Network interface.
+ * @author Artiom N.
+ * @date 22.03.2023
+ */
+
+#pragma once
+
+/// Framework namespace.
+namespace knp::framework
+{
+
+/**
+ * @brief The Network class is a neural network containing populations and projections.
+ */
+class Network
+{
+public:
+private:
+};
+
+}  // namespace knp::framework
