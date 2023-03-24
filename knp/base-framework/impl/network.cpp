4325e05bc5a9a13e4a6c5fafc8a84e0a14968ddc
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-03-24 15:30:26 +0300

    Network methods implemented

--- knp/base-framework/impl/network.cpp
+++ knp/base-framework/impl/network.cpp
@@ -13,4 +13,27 @@
 namespace knp::framework
 {
 
+void Network::add_population(Network::AllPopulationVariants &&population) {}
+
+
+void Network::add_projection(Network::AllProjectionVariants &&projection) {}
+
+
+void Network::remove_projection(const core::UID &projection_uid) {}
+
+
+void Network::remove_population(const core::UID &population_uid) {}
+
+
+Network::AllPopulationVariants &Network::get_population(const knp::core::UID &population_uid) {}
+
+
+Network::AllProjectionVariants &Network::get_projection(const knp::core::UID &projection_uid) {}
+
+
+const Network::AllPopulationVariants &Network::get_population(const knp::core::UID &population_uid) const {}
+
+
+const Network::AllProjectionVariants &Network::get_projection(const knp::core::UID &projection_uid) const {}
+
 }  // namespace knp::framework
--- knp/base-framework/include/knp/framework/network.h
+++ knp/base-framework/include/knp/framework/network.h
@@ -7,15 +7,19 @@
 
 #pragma once
 
+#include <knp/core/core.h>
 #include <knp/core/population.h>
 #include <knp/core/projection.h>
+#include <knp/framework/coordinates/generator.h>
 #include <knp/neuron-traits/all_traits.h>
 #include <knp/synapse-traits/all_traits.h>
 
 #include <vector>
 
 
-/// Framework namespace.
+/**
+ * @brief Framework namespace.
+ */
 namespace knp::framework
 {
 
@@ -32,7 +36,72 @@ public:
     using AllProjectionVariants = boost::mp11::mp_rename<AllProjections, std::variant>;
 
 public:
+    Network() = default;
+
+public:
+    /**
+     * @brief add_population add population to network.
+     * @param population new population.
+     */
+    void add_population(AllPopulationVariants &&population);
+    /**
+     * @brief add_projection add projection to network.
+     * @param projection new projection.
+     */
+    void add_projection(AllProjectionVariants &&projection);
+
+public:
+    /**
+     * @brief remove_projection remove projection from network.
+     * @param projection_uid projection UID.
+     */
+    void remove_projection(const knp::core::UID &projection_uid);
+    /**
+     * @brief remove_population remove population from network.
+     * @param population_uid population UID.
+     */
+    void remove_population(const knp::core::UID &population_uid);
+
+public:
+    /**
+     * @brief get_population get population from network.
+     * @param population_uid population UID.
+     * @throw std::logic_error if population was not found.
+     * @return existing population.
+     */
+    AllPopulationVariants &get_population(const knp::core::UID &population_uid);
+    /**
+     * @copydoc get_population
+     */
+    const AllPopulationVariants &get_population(const knp::core::UID &population_uid) const;
+
+    /**
+     * @brief get_projection get projection from network.
+     * @param projection_uid projection UID.
+     * @throw std::logic_error if projection was not found.
+     * @return existing projection.
+     */
+    AllProjectionVariants &get_projection(const knp::core::UID &projection_uid);
+    /**
+     * @copydoc get_projection
+     */
+    const AllProjectionVariants &get_projection(const knp::core::UID &projection_uid) const;
+
+public:
+    /**
+     * @brief Get the network UID.
+     * @return UID.
+     */
+    [[nodiscard]] const knp::core::UID &get_uid() const { return base_.uid_; }
+    /**
+     * @brief Get tags used by the network.
+     * @return tag map.
+     * @see TagMap.
+     */
+    [[nodiscard]] auto &get_tags() { return base_.tags_; }
+
 private:
+    knp::core::BaseData base_;
     std::vector<AllPopulationVariants> populations_;
     std::vector<AllProjectionVariants> projections_;
 };
