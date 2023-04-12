0d629ac77f5fa4d96a37d0ac23c5977a65b14771
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2023-04-12 14:41:11 +0300

    Model header improved

--- knp/base-framework/include/knp/framework/model.h
+++ knp/base-framework/include/knp/framework/model.h
@@ -9,6 +9,10 @@
 
 #include <knp/core/core.h>
 #include <knp/core/impexp.h>
+#include <knp/framework/backend_loader.h>
+#include <knp/framework/network.h>
+
+#include <memory>
 
 
 /**
@@ -18,7 +22,8 @@ namespace knp::framework
 {
 
 /**
- * @brief The Model class is a definition of a model that contains a network, input and output channels, a monitor and a backend.
+ * @brief The Model class is a definition of a model that contains a network, input and output channels, a monitor and a
+ * backend.
  */
 class Model
 {
@@ -26,6 +31,8 @@ public:
 public:
 private:
     knp::core::BaseData base_;
+    std::shared_ptr<knp::core::Backend> backend_;
+    knp::framework::Network network_;
 };
 
 }  // namespace knp::framework
