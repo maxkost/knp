cb57a8980a501a3d7d998fe6f95b573d05deacb8
Yuliya Z. Frolova <y.frolova@not-a-real-email.notreal>
2023-07-13 11:57:32 +0300

    Merged PR: Spellchecking Doxygen files

--- knp/neuron-traits-library/include/knp/neuron-traits/blifat.h
+++ knp/neuron-traits-library/include/knp/neuron-traits/blifat.h
@@ -80,7 +80,7 @@ struct neuron_parameters<BLIFATNeuron>
      */
     double min_potential_ = default_values<BLIFATNeuron>::min_potential;
     /**
-     * @brief The parameter defines the number of netwrok execution steps, during which the neuron activity is totally blocked.
+     * @brief The parameter defines the number of network execution steps, during which the neuron activity is totally blocked.
      */
     unsigned total_blocking_period_ = 0;
 };
