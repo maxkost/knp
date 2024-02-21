604acd05d148622cff9f63b6039b5d45799d68cc
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-02-21 17:26:39 +0300

    Redundant constructor removed

--- knp/python-framework/impl/core/projection.h
+++ knp/python-framework/impl/core/projection.h
@@ -22,31 +22,14 @@ struct ProjectionGeneratorProxy
 {
     explicit ProjectionGeneratorProxy(const py::object &gen_func) : gen_func_(gen_func) {}
 
-    std::optional<std::tuple<ElemParametersType, size_t, size_t>> operator()(size_t index)
+    std::optional<typename core::Projection<ElemParametersType>::Synapse> operator()(size_t index)
     {
         auto res = py::call<py::object>(gen_func_.ptr(), index);
         if (res.is_none())
         {
             return std::nullopt;
         }
-        return *reinterpret_cast<std::tuple<ElemParametersType, size_t, size_t> *>(res.ptr());
-    }
-
-    const py::object &gen_func_;
-};
-
-
-// TODO: remove this.
-template <typename ElemType>
-struct ProjectionGeneratorProxy1
-{
-    explicit ProjectionGeneratorProxy1(const py::object &gen_func) : gen_func_(gen_func) {}
-
-    std::optional<typename core::Projection<ElemType>::Synapse> operator()(size_t index)
-    {
-        auto res = py::call<py::object>(gen_func_.ptr(), index);
-        if (res.is_none()) return std::nullopt;
-        return *reinterpret_cast<typename core::Projection<ElemType>::Synapse *>(res.ptr());
+        return *reinterpret_cast<typename core::Projection<ElemParametersType>::Synapse *>(res.ptr());
     }
 
     const py::object &gen_func_;
@@ -59,8 +42,7 @@ std::shared_ptr<core::Projection<ElemType>> projection_constructor_wrapper(
     size_t num_iterations)
 {
     return std::make_shared<core::Projection<ElemType>>(
-        uid, presynaptic_uid, postsynaptic_uid,
-        ProjectionGeneratorProxy<typename core::Projection<ElemType>::SynapseParameters>(gen_func), num_iterations);
+        uid, presynaptic_uid, postsynaptic_uid, ProjectionGeneratorProxy<ElemType>(gen_func), num_iterations);
 };
 
 
@@ -69,8 +51,7 @@ std::shared_ptr<core::Projection<ElemType>> projection_constructor_wrapper(
     core::UID presynaptic_uid, core::UID postsynaptic_uid, const py::object &gen_func, size_t num_iterations)
 {
     return std::make_shared<core::Projection<ElemType>>(
-        presynaptic_uid, postsynaptic_uid,
-        ProjectionGeneratorProxy<typename core::Projection<ElemType>::SynapseParameters>(gen_func), num_iterations);
+        presynaptic_uid, postsynaptic_uid, ProjectionGeneratorProxy<ElemType>(gen_func), num_iterations);
 };
 
 
@@ -78,5 +59,5 @@ template <typename ElemType>
 void projection_synapses_add_wrapper(
     core::Projection<ElemType> &projection, const py::object &gen_func, size_t num_iterations)
 {
-    projection.add_synapses(ProjectionGeneratorProxy1<ElemType>(gen_func), num_iterations);
+    projection.add_synapses(ProjectionGeneratorProxy<ElemType>(gen_func), num_iterations);
 };
