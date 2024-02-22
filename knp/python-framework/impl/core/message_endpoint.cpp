0e104d03d1eed4429447d1660bf8a18f7c918063
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-02-22 18:12:43 +0300

    Message endpoint Python bindings partially implemented

--- knp/python-framework/impl/core/message_endpoint.cpp
+++ knp/python-framework/impl/core/message_endpoint.cpp
@@ -8,13 +8,9 @@
 
 
 #if defined(_KNP_IN_CORE)
-/*
-py::class_<core::MessageEndpoint>("MessageEndpoint", "The MessageEndpoint class is a definition of message endpoints.")
-    .def("get_receiver_uid", &core::MessageEndpoint::get_receiver_uid, "Get receiver UID from a subscription variant.")
-    .def(
-        "get_subscription_key", &core::MessageEndpoint::get_subscription_key,
-        "Get subscription key from a subscription variant.")
-    .def(py::init<core::MessageEndpoint>(), "Move constructor for message endpoints.")
+py::class_<core::MessageEndpoint, boost::noncopyable>(
+    "MessageEndpoint", "The MessageEndpoint class is a definition of message endpoints.", py::no_init)
+    //    .def(py::init<core::MessageEndpoint&&>(), "Move constructor for message endpoints.")
     //        .def(
     //            "operator=", (MessageEndpoint(MessageEndpoint::*)(knp::MessageEndpoint)) &
     //            MessageEndpoint::operator=, "Default copy operator.")
@@ -22,19 +18,22 @@ py::class_<core::MessageEndpoint>("MessageEndpoint", "The MessageEndpoint class 
     //        .def(
     //            "operator=", (MessageEndpoint(MessageEndpoint::*)(knp::MessageEndpoint)) &
     //            MessageEndpoint::operator=, "Avoid copy assignment of an endpoint.")
+    .def("get_receiver_uid", &core::MessageEndpoint::get_receiver_uid, "Get receiver UID from a subscription variant.")
     .def(
-        "subscribe", &core::MessageEndpoint::subscribe,
-        "Add a subscription to messages of the specified type from senders with given UIDs.")
-    .def("unsubscribe", &core::MessageEndpoint::unsubscribe, "Unsubscribe from messages of a specified type.")
+        "get_subscription_key", &core::MessageEndpoint::get_subscription_key,
+        "Get subscription key from a subscription variant.")
+    //    .def(
+    //        "subscribe", &core::MessageEndpoint::subscribe,
+    //        "Add a subscription to messages of the specified type from senders with given UIDs.");
+    //    .def("unsubscribe", &core::MessageEndpoint::unsubscribe, "Unsubscribe from messages of a specified type.")
     .def(
         "remove_receiver", &core::MessageEndpoint::remove_receiver,
         "Remove all subscriptions for a receiver with given UID.")
     .def("send_message", &core::MessageEndpoint::send_message, "Send a message to the message bus.")
-    .def(
-        "unload_messages", &core::MessageEndpoint::unload_messages,
-        "Read messages of the specified type received via subscription.")
+    //    .def(
+    //        "unload_messages", &core::MessageEndpoint::unload_messages,
+    //        "Read messages of the specified type received via subscription.")
     .def(
         "receive_all_messages", &core::MessageEndpoint::receive_all_messages,
         "Receive all messages that were sent to the endpoint.");
-*/
 #endif
