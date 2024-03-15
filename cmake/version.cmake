8dcabe7c19e9d0b35a979c4969ec244daf83576a
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-03-15 18:03:12 +0300

    Versioning fix

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -2,60 +2,59 @@
 # CMake version reading functions. Artiom N.(cl)2022
 #
 
-function(read_version filename)
+function(read_version_file filename)
+    message(STATUS "Reading version file...")
     file(READ "${filename}" VERSION_FILE_RAW)
-    string(STRIP "${VERSION_FILE_RAW}" VERSION_FILE)
-    string(REGEX MATCHALL "\s*[0-9]+\.[0-9]+\.[0-9]+$" VER_LIST "${version_string}")
 
-    set(PROJECT_VERSION ${VER_LIST} CACHE STRING
-        "Version determined from \"${filename}\"" FORCE
-    )
-    message(STATUS "Project version: ${PROJECT_VERSION}")
+    string(STRIP "${VERSION_FILE_RAW}" KNP_VERSION)
+    set(KNP_VERSION ${KNP_VERSION} PARENT_SCOPE)
+
+    message(STATUS "Version from file: ${KNP_VERSION}")
 endfunction()
 
 
 function(determine_version_with_git)
-    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/GetGitRevisionDescription.cmake)
-    git_describe(VERSION ${ARGN})
+    message(STATUS "Determining version from Git...")
+    execute_process(COMMAND git describe --tags
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            RESULT_VARIABLE GIT_EXECUTION_RESULT
+            OUTPUT_VARIABLE GIT_PKG_VERSION_FULL
+            ERROR_VARIABLE GIT_VERSION_ERROR
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    if(GIT_EXECUTION_RESULT)
+        message(WARNING "Could not retrieve version from command 'git describe --tags'")
+        return()
+    endif()
+
     string(FIND ${VERSION} "." VALID_VERSION)
     if(VALID_VERSION EQUAL -1)
         message(WARNING "Version string ${VERSION} retrieved with git describe is invalid")
         return()
     endif()
-    message(STATUS "${MSG_PREFIX} Version string determined with git describe: ${VERSION}")
-    # Parse the version information into pieces.
-    string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" VERSION_MAJOR "${VERSION}")
-    string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" VERSION_MINOR "${VERSION}")
-    string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" VERSION_PATCH "${VERSION}")
-    string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.[0-9]+(.*)" "\\1" VERSION_SHA1 "${VERSION}")
-    set(ETL_VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
-
-    set(ETL_VERSION ${ETL_VERSION} CACHE STRING
-        "ETL version determined with git describe" FORCE
-    )
-    message(STATUS "${MSG_PREFIX} Determined ETL version ${ETL_VERSION} from the git tag")
+    message(STATUS "Version string determined with git describe: ${VERSION}")
 endfunction()
 
-#
-#include(CMakePackageConfigHelpers)
-#
-#write_basic_package_version_file(
-#        "utilsConfigVersion.cmake"
-#        VERSION ${PROJECT_VERSION}
-#        COMPATIBILITY AnyNewerVersion
-#)
-#
-#configure_package_config_file(
-#        "${PROJECT_SOURCE_DIR}/cmake/utilsConfig.cmake.in"
-#        "${PROJECT_BINARY_DIR}/utilsConfig.cmake"
-#        INSTALL_DESTINATION
-#        ${CMAKE_INSTALL_LIBDIR}/cmake/utils
-#)
-#
-#install(
-#        FILES
-#        ${PROJECT_BINARY_DIR}/utilsConfig.cmake
-#        ${CMAKE_CURRENT_BINARY_DIR}/utilsConfigVersion.cmake
-#        DESTINATION
-#        ${CMAKE_INSTALL_LIBDIR}/cmake/utils
-#)
+
+function(get_knp_version)
+    determine_version_with_git()
+
+    if (NOT KNP_VERSION)
+        read_version_file("${CMAKE_SOURCE_DIR}/VERSION")
+    endif()
+
+    message(STATUS "KNP version: ${KNP_VERSION}")
+
+    if (NOT KNP_VERSION)
+        message(WARNING "KNP version can't be determined!")
+        return()
+    endif()
+
+    # Parse the version information into pieces.
+    string(REGEX REPLACE "v?([0-9]+\\.[0-9]+(\\.[0-9]+)?).*"  "\\1"
+           KNP_VERSION "${KNP_VERSION}")
+
+    set(KNP_VERSION "${KNP_VERSION}" PARENT_SCOPE)
+
+    # set(KNP_VERSION ${KNP_VERSION} CACHE STRING "KNP version" FORCE)
+endfunction()
