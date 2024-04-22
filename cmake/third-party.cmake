cab263b0e772e8f431229f9508aa3c74fa5a5eaf
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-04-22 20:40:54 +0300

    Dependencies used CPM

--- cmake/third-party.cmake
+++ cmake/third-party.cmake
@@ -4,9 +4,13 @@
 
 include_guard(GLOBAL)
 
+include(CPM)
+set(CPM_)
+# include("git")
 
-include("git")
 
 function(add_third_party module_name)
-    add_git_submodule("${CMAKE_CURRENT_SOURCE_DIR}/third-party/${module_name}")
+#    add_git_submodule("${CMAKE_CURRENT_SOURCE_DIR}/third-party/${module_name}")
+    set(CPM_USE_LOCAL_PACKAGES ON)
+    CPMADDPackage("${module_name}" ${ARGN})
 endfunction()
