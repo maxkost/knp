81db6bc14964e22e14485f137e1957d7e5df293f
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-04-25 12:13:03 +0300

    Python requirements.txt extended, CLang tidy enabled

--- cmake/knp-functions.cmake
+++ cmake/knp-functions.cmake
@@ -4,21 +4,6 @@
 
 include_guard(GLOBAL)
 
-if(KNP_COTIRE_ENABLED)
-    include(cotire)
-endif()
-
-function(knp_capitalize_string src result)
-    # Get first letter and capitalize.
-    string(SUBSTRING ${src} 0 1 first-letter)
-    string(TOUPPER ${first-letter} first-letter)
-    # Get the rest of the macro name.
-    string(LENGTH ${src} length)
-    string(SUBSTRING ${src} 1 ${length} rest)
-    # Compose macro-name with first letter uppercase.
-    set(${result} "${first-letter}${rest}" PARENT_SCOPE)
-endfunction()
-
 
 function(knp_set_target_parameters target)
     target_compile_options("${target}" PRIVATE $<$<COMPILE_LANG_AND_ID:C,Clang>:-Wdocumentation>)
@@ -27,12 +12,29 @@ function(knp_set_target_parameters target)
     target_compile_options("${target}" PUBLIC $<$<COMPILE_LANG_AND_ID:C,GNU,Clang>:-Wall -fstack-protector-all -Wformat -Wformat-security>)
     target_compile_options("${target}" PUBLIC $<$<COMPILE_LANG_AND_ID:CXX,GNU,Clang>:-Wall -fstack-protector-all -Wformat -Wformat-security>)
 
-    target_compile_definitions("${target}" PRIVATE "KNP_LIBRARY_NAME=${target}")
+    target_compile_definitions("${target}" PRIVATE
+                               "KNP_LIBRARY_NAME=${target}")
+    target_compile_features("${target}" PUBLIC cxx_std_17)
 
-    target_include_directories("${lib_name}" PUBLIC
+#    target_compile_definitions("${target}" PRIVATE
+#                                $<$<CONFIG:Debug>:_FORTIFY_SOURCE=2>
+#                                $<$<CONFIG:Release>:_FORTIFY_SOURCE=1>)
+    # Sanitizer (dynamic analysis).
+    # Sanitizers don't work under TFS. Temporarily disabled.
+
+    # set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=address -fsanitize-recover=address")
+    # set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=address -fsanitize-recover=address")
+    if(UNIX AND NOT APPLE)
+        #        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-z,noexecstack -Wl,-z,relro,-z,now")
+    endif()
+
+    target_include_directories("${target}" PUBLIC
             "$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>"
             "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>")
 
+    if(KNP_PRECOMP_ENABLED)
+#        target_precompile_headers("${target}" PRIVATE impl/precompiled_header.h)
+    endif()
 endfunction()
 
 
@@ -49,11 +51,6 @@ function (_knp_add_library lib_name lib_type)
         python3_add_library("${lib_name}" MODULE ${${lib_name}_source})
     else()
         add_library("${lib_name}" ${lib_type} ${${lib_name}_source})
-
-        if(KNP_COTIRE_ENABLED)
-            # Not used for Python modules.
-            cotire("${lib_name}")
-        endif()
     endif()
 
     knp_set_target_parameters("${lib_name}")
@@ -114,61 +111,6 @@ function (knp_add_library lib_name lib_type)
 endfunction()
 
 
-function (knp_set_backend_common_parameters BACKEND_NAME PROJECT_ROOT_DIR)
-    string(TOLOWER "${BACKEND_NAME}" BACKEND_DIRECTORY)
-    file(REAL_PATH "${PROJECT_ROOT_DIR}/src" PROJECT_SOURCE_DIR)
-    file(REAL_PATH "${PROJECT_SOURCE_DIR}/impl/backends/${BACKEND_DIRECTORY}" BACKEND_SUBDIR)
-    knp_capitalize_string(${BACKEND_DIRECTORY} BACKEND_CLASS_PREFIX)
-    target_compile_definitions("knp_backend_${BACKEND_DIRECTORY}" PRIVATE "-D_BUILD_LIBRARY" PUBLIC "-DKNP_LIBRARY_BACKEND_NAME=\"${BACKEND_NAME}\"" "-DKNP_LIBRARY_BACKEND_CLASS=calculation_backend::${BACKEND_CLASS_PREFIX}CalculationBackend")
-    target_include_directories("knp_backend_${BACKEND_DIRECTORY}" PRIVATE "${BACKEND_SUBDIR}")
-    target_include_directories("knp_backend_${BACKEND_DIRECTORY}" PUBLIC "${PROJECT_SOURCE_DIR}/include")
-    target_include_directories("knp_backend_${BACKEND_DIRECTORY}" PUBLIC "${PROJECT_SOURCE_DIR}/include/snn-library/backends/${BACKEND_DIRECTORY}")
-    target_compile_features("${PROJECT_NAME}" PUBLIC cxx_std_17)
-    string(TOUPPER "${BACKEND_NAME}" BACKEND_NAME_UPPER)
-    set("BUILD_${BACKEND_NAME_UPPER}_BACKEND" 1 CACHE INTERNAL "")
-endfunction()
-
-
-macro (knp_set_cmake_common_parameters)
-    # find_package(Intl REQUIRED)
-    set(CMAKE_CXX_STANDARD 17)
-    set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
-    set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
-
-    find_package(Boost 1.74.0 REQUIRED)
-
-    if(Boost_FOUND)
-        include_directories(${Boost_INCLUDE_DIRS})
-    endif()
-
-    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -D_FORTIFY_SOURCE=2")
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -D_FORTIFY_SOURCE=2")
-
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
-
-    # Sanitizer (dynamic analysis).
-    # Sanitizers don't work under TFS. Temporarily disabled.
-
-    # set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=address -fsanitize-recover=address")
-    # set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=address -fsanitize-recover=address")
-    if(UNIX AND NOT APPLE)
-#        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,-z,noexecstack -Wl,-z,relro,-z,now")
-    endif()
-endmacro()
-
-
-function(CUDA_CONVERT_FLAGS EXISTING_TARGET)
-    get_property(old_flags TARGET ${EXISTING_TARGET} PROPERTY INTERFACE_COMPILE_OPTIONS)
-    if(NOT "${old_flags}" STREQUAL "")
-        string(REPLACE ";" "," CUDA_flags "${old_flags}")
-        set_property(TARGET ${EXISTING_TARGET} PROPERTY INTERFACE_COMPILE_OPTIONS
-            "$<$<BUILD_INTERFACE:$<COMPILE_LANGUAGE:CXX>>:${old_flags}>$<$<BUILD_INTERFACE:$<COMPILE_LANGUAGE:CUDA>>:-Xcompiler=${CUDA_flags}>"
-            )
-    endif()
-endfunction()
-
-
 # Function:                 knp_add_python_library
 # Description:              Add Python module, using Boost::Python.
 # Param name:               Module name.
@@ -176,7 +118,6 @@ endfunction()
 # Requires:
 #     find_package(Python COMPONENTS Interpreter Development REQUIRED)
 #     find_package(Boost 1.66.0 COMPONENTS python REQUIRED)
-
 function(knp_add_python_module name)
     cmake_parse_arguments(
             "PARSED_ARGS"
