5e2feb919875de0e227719435431ccc42d4fc129
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-09-02 21:16:33 +0300

    Coordinates implemented

--- knp/base-framework/include/knp/framework/coordinates/cartesian.h
+++ knp/base-framework/include/knp/framework/coordinates/cartesian.h
@@ -9,23 +9,52 @@
 
 #pragma once
 
+#include <boost/geometry.hpp>
+
 
 /**
- * @brief Namespace for framework coordinates.
+ * @brief Namespace for the cartesian framework coordinates.
  */
-namespace knp::framework::coordinates
+namespace knp::framework::coordinates::cartesian
 {
 
-// struct PolarCoordinates3DTag
-//{
-//     const std::string tag_name = "polar_coordinates_3d";
+namespace d2
+{
+/**
+ * @brief Predefined tag name for the one coordinate.
+ */
+constexpr char coordinate_tag_name[]{"ortho_coordinate_2d"};
+/**
+ * @brief Predefined tag name for the coordinates array.
+ */
+constexpr char coordinate_array_tag_name[]{"ortho_coordinates_2d_array"};
 
-//    struct
-//    {
-//        double radius;
-//        double angle;
-//    } value_structure;
-//};
+/**
+ * @brief 2D point coordinates.
+ * @tparam CoordinateType coordinate value type.
+ */
+template <typename CoordinateType>
+using coordinate = boost::geometry::model::d2::point_xy<CoordinateType>;
+}  // namespace d2
 
 
-}  // namespace knp::framework::coordinates
+namespace d3
+{
+/**
+ * @brief Predefined tag name for the one coordinate.
+ */
+constexpr char coordinate_tag_name[]{"ortho_coordinate_3d"};
+/**
+ * @brief Predefined tag name for the coordinates array.
+ */
+constexpr char coordinate_array_tag_name[]{"ortho_coordinates_3d_array"};
+
+/**
+ * @brief 3D point coordinates.
+ * @tparam CoordinateType coordinate value type.
+ */
+template <typename CoordinateType>
+using coordinate = boost::geometry::model::d3::point_xyz<CoordinateType>;
+}  // namespace d3
+
+}  // namespace knp::framework::coordinates::cartesian
