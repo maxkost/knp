b73ce5a849adc24867fad40b0ceb7b52ff6f07e7
Andrey Vartenkov <a.vartenkov@not-a-real-email.notreal>
2024-09-24 13:17:08 +0300

    Fixing comments in graph physics

--- examples/mnist-client/graph_physics.cpp
+++ examples/mnist-client/graph_physics.cpp
@@ -1,7 +1,6 @@
 /**
  * @file graph_physics.cpp
  * @brief Implement graph physics.
- * @author A. Vartenkov
  * @date 21.08.2024
  * @license Apache 2.0
  * @copyright © 2024 AO Kaspersky Lab
@@ -16,7 +15,9 @@
 #include <opencv2/core.hpp>
 
 
-VisualGraph::VisualGraph(const std::vector<int> &nodes, const AdjacencyList &adj_list) : base_graph_(adj_list)
+// Construct graph class from an adjacency list.
+VisualGraph::VisualGraph(const std::vector<int> &nodes, const std::vector<std::vector<size_t>> &adj_list)
+    : base_graph_(adj_list)
 {
     edges_mat_.resize(base_graph_.size());
     for (auto &edge : edges_mat_) edge.resize(base_graph_.size());
@@ -30,21 +31,27 @@ VisualGraph::VisualGraph(const std::vector<int> &nodes, const AdjacencyList &adj
 }
 
 
+// We show graph using a physical model: all nodes repel each other, all connected nodes are connected as if by
+// springs of a fixed length. There is also a drag force to dampen oscillations, it's calculated independently.
+// This function calculates forces between two nodes.
 cv::Vec2d VisualGraph::get_force(const PhysicsPoint &target, const PhysicsPoint &influence, bool has_edge) const
 {
     double very_small_number = 1e-5;
     cv::Vec2d dist = target.pos_ - influence.pos_;
     double len = cv::norm(dist);
     cv::Vec2d force = 0;
-    if (has_edge) force += -dist * (len - spring_len) / std::max(len, very_small_number) * spring_strength;
-    force += dist / (len * len * len + very_small_number) * repel_coeff;
+    if (has_edge) force += -dist * (len - spring_len_) / std::max(len, very_small_number) * spring_strength_;
+    force += dist / (len * len * len + very_small_number) * repel_coeff_;
     return force;
 }
 
 
+// A single iteration recalculates forces and velocities then moves the points according to their corresponding
+// velocities.
 void VisualGraph::iterate()
 {
     double dt = 0.2;
+    // Calculate forces
     std::vector<cv::Vec2d> forces;
     forces.resize(points_.size(), {0.0, 0.0});
     for (size_t i = 0; i < points_.size(); ++i)
@@ -55,8 +62,10 @@ void VisualGraph::iterate()
             auto force = get_force(points_[i], points_[j], edges_mat_[i][j] || edges_mat_[j][i]);
             forces[i] += force;
         }
+        // Drag
         forces[i] += -points_[i].vel_ * resistance_;
     }
+    // Update velocities and positions.
     for (size_t i = 0; i < points_.size(); ++i)
     {
         points_[i].vel_ += forces[i] * dt;
@@ -65,17 +74,18 @@ void VisualGraph::iterate()
 }
 
 
+// Iterate N times.
 void VisualGraph::iterate(int n)
 {
     for (int i = 0; i < n; ++i) iterate();
 }
 
 
+// Get all points positions.
 [[nodiscard]] std::vector<cv::Point2d> VisualGraph::get_positions() const
 {
     std::vector<cv::Point2d> result;
     result.reserve(points_.size());
-    // for (const auto &point : points_) result.push_back(cv::Point2d(point.pos_[0], point.pos_[1]));
     std::transform(
         points_.begin(), points_.end(), std::back_inserter(result),
         [](const auto &v) { return cv::Point2d(v.pos_[0], v.pos_[1]); });
@@ -83,6 +93,7 @@ void VisualGraph::iterate(int n)
 }
 
 
+// Rescale a graph to make it fit inside a rectangle.
 std::vector<cv::Point2i> VisualGraph::scale_graph(const cv::Size &screen_size, int margin) const
 {
     const double small_num = 1e-5;
@@ -90,6 +101,8 @@ std::vector<cv::Point2i> VisualGraph::scale_graph(const cv::Size &screen_size, i
     cv::Point2d high_point{std::numeric_limits<double>::min(), std::numeric_limits<double>::min()};
     cv::Size graph_max_size = screen_size - cv::Size{2 * margin, 2 * margin};
     auto points = get_positions();
+
+    // Maybe do a rotation here later, probably using cv::minAreaRect.
     for (const auto &point : points)
     {
         low_point.x = std::min(low_point.x, point.x);
@@ -97,13 +110,12 @@ std::vector<cv::Point2i> VisualGraph::scale_graph(const cv::Size &screen_size, i
         high_point.x = std::max(high_point.x, point.x);
         high_point.y = std::max(high_point.y, point.y);
     }
+
     cv::Size2d actual_size(cv::Point2d{high_point.x - low_point.x, high_point.y - low_point.y});
-    double scale_x = 1.0 * graph_max_size.width / (actual_size.width + small_num);
-    double scale_y = 1.0 * graph_max_size.height / (actual_size.height + small_num);
-    // maybe do a rotation here later
+    double scale_x = static_cast<double>(graph_max_size.width) / (actual_size.width + small_num);
+    double scale_y = static_cast<double>(graph_max_size.height) / (actual_size.height + small_num);
 
-    // rescale to fit dimensions
-
+    // Rescale to fit dimensions.
     double scale = std::min(scale_x, scale_y);
     std::vector<cv::Point2i> result;
     result.reserve(points.size());
--- examples/mnist-client/graph_physics.h
+++ examples/mnist-client/graph_physics.h
@@ -12,48 +12,78 @@
 #include <opencv2/core.hpp>
 
 
-using AdjacencyList = std::vector<std::vector<size_t>>;
-
-
+/**
+ * @brief A point that has position and velocity.
+ */
 struct PhysicsPoint
 {
     // cppcheck-suppress unusedStructMember
+    /**
+     * @brief Point index.
+     */
     int index_;
+
+    /**
+     * @brief Point position.
+     */
     cv::Vec2d pos_;
+
+    /**
+     * @brief Point velocity.
+     */
     cv::Vec2d vel_;
 };
 
 
+/**
+ * Graph description with additional parameters used for visualization.
+ */
 class VisualGraph
 {
 public:
-    // Construct graph visualizer.
-    // nodes - subgraph node indexes.
-    // adj_list - whole graph adjacency list.
-    explicit VisualGraph(const std::vector<int> &nodes, const AdjacencyList &adj_list);
+    /**
+     * @brief Construct graph visualizer.
+     * @param nodes subgraph node indexes.
+     * @param adj_list whole graph adjacency list.
+     */
+    explicit VisualGraph(const std::vector<int> &nodes, const std::vector<std::vector<size_t>> &adj_list);
 
-    // Calculate forces, modify point velocities and move points around.
+    /**
+     * @brief Calculate forces, modify point velocities and move points around.
+     */
     void iterate();
 
-    // iterate() n times.
+    /**
+     * @brief Iterate N times.
+     * @param n number of iterations.
+     */
     void iterate(int n);
 
-    // Fit graph to image size, get point coordinates for the image.
+    /**
+     * @brief Fit graph to image size, get point coordinates for the image.
+     * @param screen_size output rectangle size.
+     * @param margin margin sizes.
+     */
     [[nodiscard]] std::vector<cv::Point2i> scale_graph(const cv::Size &screen_size, int margin) const;
 
-    // Get point positions
+    /**
+     * @brief Get points positions.
+     * @return points positions.
+     */
     [[nodiscard]] std::vector<cv::Point2d> get_positions() const;
 
 private:
     // Data
-    AdjacencyList base_graph_;
+    std::vector<std::vector<size_t>> base_graph_;
+    // Edges (true = has an edge).
     std::vector<std::vector<bool>> edges_mat_;
+    // Positions and velocities of the nodes.
     std::vector<PhysicsPoint> points_;
 
     // Hyperparameters
-    double spring_strength = 1.0f;
-    double spring_len = 1.0f;
-    double repel_coeff = 0.3f;
+    double spring_strength_ = 1.0f;
+    double spring_len_ = 1.0f;
+    double repel_coeff_ = 0.3f;
     double resistance_ = 1.0f;
 
     // Calculates force from one point to another. Force consists of "repel", "spring" and "resistance" components.
