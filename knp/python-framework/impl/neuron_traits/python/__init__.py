830d9795ebd5d461b4e71f0c6cfc36fa41deffa5
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-09-30 21:35:22 +0300

    PyLint fix

--- knp/python-framework/impl/neuron_traits/python/__init__.py
+++ knp/python-framework/impl/neuron_traits/python/__init__.py
@@ -1,8 +1,9 @@
 """Import knp.neuron_traits."""
 
+# pylint: disable = no-name-in-module
 from knp.neuron_traits._knp_python_framework_neuron_traits import (
     BLIFATNeuronParameters,
-)  # pylint: disable = no-name-in-module
+)
 
 __all__ = [
     'BLIFATNeuronParameters',
