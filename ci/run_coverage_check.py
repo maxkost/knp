72fce149fa799cf2ba5a52d043166739b2883028
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-10-14 15:41:44 +0300

    Coverage script fix

--- ci/run_coverage_check.py
+++ ci/run_coverage_check.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 from pathlib import Path
 
+
 if len(sys.argv) < 3:
     print(f'{Path(__file__).name} <root directory> <percent>')
     sys.exit(1)
@@ -17,6 +18,8 @@ command = [
     '-r',
     str(root_dir),
     '-e',
+    str(Path('.*usr') / 'include'),
+    '-e',
     str(root_dir / 'third-party' / '.*'),
     '-e',
     str(root_dir / 'lib' / 'third-party' / '.*'),
@@ -25,7 +28,9 @@ command = [
     '-e',
     str(root_dir / 'knp' / '.*-traits-library'),
     '-e',
-    '.*\\.h',
+    str(root_dir / 'examples'),
+    '--filter',
+    'knp',
     '--json-summary',
     '--gcov-ignore-parse-errors',
 ]
