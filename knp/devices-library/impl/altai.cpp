67d34a0dcb1b6bfff3f4de57ee00d42c8c07e6a5
Artiom N. <a.nechiporuk@not-a-real-email.notreal>
2024-10-30 16:20:55 +0300

    Motiv authors set

--- knp/devices-library/impl/altai.cpp
+++ knp/devices-library/impl/altai.cpp
@@ -1,8 +1,22 @@
 /**
  * @file altai.cpp
  * @brief AltAI devices class implementation.
- * @kaspersky_support Liubiakin A.
+ * @author Liubiakin A.
  * @date 17.10.2024
+ * @license Apache 2.0
+ * @copyright © 2024 Motiv NT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <knp/devices/altai.h>
