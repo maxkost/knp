d41594903580378834a415fd2f78bf8d2888de17
Andrey Vartenkov <a.vartenkov@not-a-real-email.notreal>
2024-10-31 14:58:18 +0300

    Add functions to model loader

--- knp/python-framework/impl/base_framework/cpp/model_loader.cpp
+++ knp/python-framework/impl/base_framework/cpp/model_loader.cpp
@@ -17,7 +17,18 @@ py::def("make_model_loader", &make_model_loader, "Create model loader.");
 py::class_<knp::framework::ModelLoader, boost::noncopyable>(
     "ModelLoader", "The ModelLoader class is a definition of a loader that uploads the model to the specified backend.",
     py::no_init)
-    .def("load", &knp::framework::ModelLoader::load, "Write model to backend.");
-// TODO: Add input/output channels
+    .def("load", &knp::framework::ModelLoader::load, "Write model to backend.")
+    .def(
+        "get_inputs", &knp::framework::ModelLoader::get_inputs, py::return_internal_reference<>(),
+        "Get input channels.")
+    .def(
+        "get_outputs", &knp::framework::ModelLoader::get_outputs, py::return_internal_reference<>(),
+        "Get output channels.")
+    .def(
+        "get_input_channel", get_model_loader_input_channel, py::return_internal_reference<>(),
+        "Get input channel reference.")
+    .def(
+        "get_output_channel", get_model_loader_output_channel, py::return_internal_reference<>(),
+        "Get output channel reference.");
 
 #endif  // KNP_IN_BASE_FW
